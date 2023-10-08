4b0cd985e0aa3a6f97d836fed0c3d6f965d33726
Rui Shan <ruishan.teo03@gmail.com>
2023-10-08 09:46:43 +0800

    Update image picker permissions

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
-export const API_URL =
-  process.env.NODE_ENV === "development"
-    ? process.env.EXPO_PUBLIC_LOCAL_API_URL
-    : process.env.EXPO_PUBLIC_API_URL;
+export const SPOON_API_URL = process.env.EXPO_PUBLIC_SPOON_API_URL;
+export const BACKEND_API_URL = process.env.EXPO_PUBLIC_BACKEND_API_URL;
 
-export const API_KEY = process.env.EXPO_PUBLIC_API_KEY;
-export const FAKE_API = process.env.EXPO_PUBLIC_FAKE_API;
+export const API_KEY = process.env.EXPO_PUBLIC_SPOON_API_KEY;
+export const FAKE_API = false;
+
+export const GOOGLE_API_KEY = process.env.EXPO_PUBLIC_GOOGLE_API_KEY;
